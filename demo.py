fc89c3d491374abd539bb0ebf0119788647eca85
elaina <userelaina@pm.me>
2025-01-16 15:28:02 +0000

    :bug:

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import numpy as np
-import x256numpy as x256
+import x256offline
+import x256numpy
 
 y, x = 96, 54
 img = np.random.randint(0, 256, (x, y, 3), dtype=np.uint8)
@@ -20,15 +21,15 @@ img = img.astype(np.int32)
 for color in COLOR_X256_LIST:
     weighted = color in [COLOR_X256W, COLOR_X232W]
     n_color = 232 if color in [COLOR_X232E, COLOR_X232W] else 256
-    ans = x256.from_rgb(img[:, :, 2], img[:, :, 1], img[:, :, 0], weighted, n_color)
+    ans = x256numpy.from_rgb(img[:, :, 0], img[:, :, 1], img[:, :, 2], weighted, n_color)
     print(ans.shape)
     for j in range(x):
         for k in range(y):
             b, g, r = img[j, k]
-            _l = x256.from_rgb(
+            _l = x256offline.from_rgb(
                 r, g, b, weighted, n_color
             )
             _r = ans[j, k]
-            assert int(_l) == int( _r), (color, j, k, _l, ans[:, j, k])
+            assert int(_l) == int( _r), (color, j, k, _l, ans[j, k])
         # print(j)
     print(color, 'OK')
